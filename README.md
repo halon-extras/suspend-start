6c7b4027b4a1ecc0f6191f0b69ef835c61bed7d6
Erik Lax <erik@halon.se>
2023-10-18 22:30:25 +0200

    Package

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 This plugin will start the queue paused. Use the API to remove the suspends by ID.
 
+## Installation
+
+Follow the [instructions](https://docs.halon.io/manual/comp_install.html#installation) in our manual to add our package repository and then run the below command.
+
+### Ubuntu
+
 ```
+apt-get install halon-extras-suspend-start
+```
+
+### RHEL
+
+```
+yum install halon-extras-suspend-start
+```
+
+## Commands
+
+```
+# halonctl plugin command suspend-start start
 # halonctl plugin command suspend-start getid
 ```
--- suspend-start.cpp
+++ suspend-start.cpp
@@ -2,6 +2,7 @@
 #include <syslog.h>
 #include <memory>
 #include <cstring>
+#include <cstdlib>
 
 char* id = nullptr;
 
@@ -14,18 +15,33 @@ int Halon_version()
 HALON_EXPORT
 bool Halon_init(HalonInitContext* hic)
 {
-	id = HalonMTA_queue_suspend_add(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, "START_SUSPEND", 0);
+	id = HalonMTA_queue_suspend_add(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, "SUSPEND_START", 0);
 	if (!id)
 		return false;
-	syslog(LOG_INFO, "startpaused: %s", id);
+	syslog(LOG_INFO, "suspend start: %s", id);
 	return true;
 }
 
 HALON_EXPORT
-bool Halon_plugin_command(const char* in, size_t len, char** out, size_t* olen)
+bool Halon_command_execute(HalonCommandExecuteContext* hcec, size_t argc, const char* argv[], size_t argvl[], char** out, size_t* outlen)
 {
-	*out = strdup(id);
-	return true;
+	if (argc > 0 && strcmp(argv[0], "getid") == 0)
+	{
+		if (!id)
+			return false;
+		*out = strdup(id);
+		return true;
+	}
+	if (argc > 0 && strcmp(argv[0], "start") == 0)
+	{
+		if (!id)
+			return false;
+		bool r = HalonMTA_queue_suspend_delete(id);
+		free(id);
+		id = nullptr;
+		return r;
+	}
+	return false;
 }
 
 HALON_EXPORT
